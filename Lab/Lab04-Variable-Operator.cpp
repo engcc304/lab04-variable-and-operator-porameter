5d0bbd9103684f8f6a6affa765aa7c93a428959b
porameter <paramet_su66@live.rmutl.ac.th>
2023-08-16 12:12:37 +0700

    update lab04

--- Lab/Lab04-Variable-Operator.cpp
+++ Lab/Lab04-Variable-Operator.cpp
@@ -34,17 +34,17 @@ int main() {
     float salaryperhours ;
     float salary ;
 
-    printf ( "Input the Employees ID:\n" ) ;
-    scanf ( "%s",EmployeeID ) ;
-    printf ( "Input the working hrs:\n" ) ;
-    scanf ( "%f",&hours ) ;
-    printf ( "Salary amount/hr:\n" ) ;
-    scanf ( "%f",&salaryperhours ) ;
+    printf( "Input the Employees ID:\n" ) ;
+    scanf( "%s",EmployeeID ) ;
+    printf( "Input the working hrs:\n" ) ;
+    scanf( "%f",&hours ) ;
+    printf( "Salary amount/hr:\n" ) ;
+    scanf( "%f",&salaryperhours ) ;
 
     salary = salaryperhours * hours ;
-    printf ( "Expected Output:\n" ) ;
-    printf ( "Employees ID = %s\n" , EmployeeID ) ;
-    printf ( "Salary = U$ %0.2f\n" , salary ) ;
+    printf( "Expected Output:\n" ) ;
+    printf( "Employees ID = %s\n" , EmployeeID ) ;
+    printf( "Salary = U$ %0.2f\n" , salary ) ;
 
-    return 0;
+    return 0 ;
 }//end main function
